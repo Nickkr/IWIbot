c530bf78d1eb50b2ffbff8d2be30744ab7f4e98e
Hen <erharthe@gmail.com>
2017-03-30 20:30:13 +0200

    Webapp conneted to Watson conversation

--- IWIbotWebServer/public/javascripts/main.js
+++ IWIbotWebServer/public/javascripts/main.js
@@ -1,4 +1,4 @@
-﻿var openWhiskWebUrl = 'https://openwhisk.ng.bluemix.net/api/v1/web/erhe1011%40hs-karlsruhe.de_iwibot/default/home.json';
+﻿var openWhiskWebUrl = 'https://openwhisk.ng.bluemix.net/api/v1/web/erhe1011%40hs-karlsruhe.de_iwibot/default/home.text';
 var openWhiskUrl = 'https://openwhisk.ng.bluemix.net/api/v1/namespaces/erhe1011%40hs-karlsruhe.de_iwibot/actions/home';
 var username = '80bb0dcc-75e6-4885-80da-7d86d121f3f6';
 var password = 'K13yYnYPk65VCCYuatfhWnKQvzTwAHYdzpADjQWHBTYSkFmRGLuu7u8Cz52N1yHH';
@@ -9,20 +9,24 @@ $(document).ready(function () {
         
         event.preventDefault();
 
-        var text = $('messageField').val();
+        var text = $('#messageField').val();
 
+        $('#chat').append('<div class="msg myMsg"><div>' + text + '<div></div>');
+        
+        console.log(text);
         $.ajax({
             beforeSend: function (xhr) {
                 //xhr.setRequestHeader("Authorization", "Basic " + btoa(username + ":" + password));
             },
             url: openWhiskWebUrl,
             method: "POST",
-            crossDomain: true,
-            data: { message: "TEst" }
+            dataType: "jsonp",
+            data: { message: text }
         })
             .done(function (data) {
-                alert(data);
-                console.log('DONE');
+                console.log(data);
+
+                $('#chat').append('<div class="msg"><div>' + data.output.text + '<div></div>');
             });
     })
 
