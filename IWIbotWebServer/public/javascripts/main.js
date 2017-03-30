b09537b82493bb6bc420d325b7f1df55c361e596
Hen <erharthe@gmail.com>
2017-03-30 02:46:08 +0200

    Connecting to OpenWhisk, Cross-Origin not jet working

--- IWIbotWebServer/public/javascripts/main.js
+++ IWIbotWebServer/public/javascripts/main.js
@@ -1,1 +1,29 @@
-﻿
+﻿var openWhiskWebUrl = 'https://openwhisk.ng.bluemix.net/api/v1/web/erhe1011%40hs-karlsruhe.de_iwibot/default/home.json';
+var openWhiskUrl = 'https://openwhisk.ng.bluemix.net/api/v1/namespaces/erhe1011%40hs-karlsruhe.de_iwibot/actions/home';
+var username = '80bb0dcc-75e6-4885-80da-7d86d121f3f6';
+var password = 'K13yYnYPk65VCCYuatfhWnKQvzTwAHYdzpADjQWHBTYSkFmRGLuu7u8Cz52N1yHH';
+
+$(document).ready(function () {
+
+    $('#chatForm').submit(function (event) {
+        
+        event.preventDefault();
+
+        var text = $('messageField').val();
+
+        $.ajax({
+            beforeSend: function (xhr) {
+                //xhr.setRequestHeader("Authorization", "Basic " + btoa(username + ":" + password));
+            },
+            url: openWhiskWebUrl,
+            method: "POST",
+            crossDomain: true,
+            data: { message: "TEst" }
+        })
+            .done(function (data) {
+                alert(data);
+                console.log('DONE');
+            });
+    })
+
+});
