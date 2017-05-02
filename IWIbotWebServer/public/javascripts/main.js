9525b9b7963344e07e39b712c3860bafc5e2b1f4
Niklas Kreutzarek <nkreutzarek@hotmail.de>
2017-05-02 14:01:29 +0200

    added conversation function

--- IWIbotWebServer/public/javascripts/main.js
+++ IWIbotWebServer/public/javascripts/main.js
@@ -1,16 +1,15 @@
 var openWhiskWebUrl = 'https://openwhisk.ng.bluemix.net/api/v1/web/erhe1011%40hs-karlsruhe.de_iwibot/default/home.http';
 var context = {};
 
+
 $(document).ready(function () {
     $('#chatForm').submit(function (event) {
 
         event.preventDefault();
 
         var text = $('#messageField').val();
-
-        $('#chat div.container').append('<div class="row msg"><div class="col-lg-5"></div><div class="col-lg-4"><div class="msg-send">' + text +'</div></div><div class="col-lg-3"></div></div>');
-        console.log($('#chat div.container').children().last());
-        $('#chat div.container').children().last().fadeIn("slow");
+        var msgSend = '<div class="row msg "><div class="col-lg-5"></div><div class="col-lg-4"><div class="msg-send">' + text +'</div></div><div class="col-lg-3"></div></div>';
+        $(msgSend).appendTo("#chat div.container").hide().fadeIn("slow");
         window.scrollTo(0,document.body.scrollHeight);
         console.log(context);
 
@@ -25,24 +24,24 @@ $(document).ready(function () {
             .done(function (data) {
                 console.log(data);
                 context = data.context;
-                $('#chat div.container').append('<div class="row msg"><div class="col-lg-3"></div><div class="col-lg-4"><div class="msg-recived">' + data.output.text +'</div></div><div class="col-lg-5"></div></div>');
-                $('#chat div.container').children().last().fadeIn("slow");
+                var msgRecived = '<div class="row msg "><div class="col-lg-3"></div><div class="col-lg-4"><div class="msg-recived">' + data.output.text +'</div></div><div class="col-lg-5"></div></div>';
+                $(msgRecived).appendTo("#chat div.container").hide().fadeIn("slow");
                 window.scrollTo(0,document.body.scrollHeight);
          });
     })
 
 });
 $(document).ready(function(){
-    $("#chat").css('padding-top',$("header").height() - 40.0);
-    console.log($("header").height() + "hashdasd");
-    $(window).resize(function(){
-        $("#chat").css('padding-top',$("header").height() -40.0);
-    });
+  $("#chat").css('padding-top',$("header").height() - 40.0);
+   console.log($("header").height() + "hashdasd");
+   $(window).resize(function(){
+   $("#chat").css('padding-top',$("header").height() -40.0);
+   });
 });
 $(document).ready(function(){
-    $(".content").css('padding-top',$("header").height() + 150.0);
-    console.log($("header").height() + "hashdasd");
+   $(".content").css('padding-top',$("header").height() + 100.0);
+   console.log($("header").height() + "hashdasd");
     $(window).resize(function(){
-        $(".content").css('padding-top',$("header").height() + 150.0);
+      $(".content").css('padding-top',$("header").height() + 100.0);
     });
 });
