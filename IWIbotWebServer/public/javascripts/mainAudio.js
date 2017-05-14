45727294c72e5edf4d51eaa38ee3fde84c61d6f8
Armin Kunkel <armin@kunkel24.de>
2017-05-14 18:59:40 +0200

    Umstellung auf MediaRecorder zur aufnahme von audio/ogg; codecs=opus mit 16kbps

--- IWIbotWebServer/public/javascripts/mainAudio.js
+++ IWIbotWebServer/public/javascripts/mainAudio.js
@@ -1,84 +1,80 @@
 var recording = false;
+var mediaRecorder = null;
 
 $(function(){
+    var recordingButton = $(".btn-circle");
+    function gotStream(stream) {
+        var options = {
+            audioBitsPerSecond : 16000,
+            mimeType : 'audio/ogg; codecs=opus'
+        }
+        mediaRecorder = new MediaRecorder(stream, options);
+
+        mediaRecorder.onstop = function (e) {
+
+        };
+
+        mediaRecorder.ondataavailable = function (e) {
+            var reader = new window.FileReader();
+            console.log('Blob size: ' + e.data.size);
+            reader.readAsDataURL(e.data);
+            reader.onloadend = function() {
+                var json = JSON.stringify({"payload": reader.result.split(',')[1]});
+                $("#mainDiv").addClass("loader");
+                recordingButton.hide();
+
+                $.ajax({
+                    url: 'https://openwhisk.ng.bluemix.net/api/v1/web/Hochschule_Test/default/Router.http',
+                    type: 'POST',
+                    data: json,
+                    contentType: "application/json",
+                    processData: false,
+                    success: function(data) {
+                        console.log("Success: " + data);
+                        var obj = JSON.parse(data);
+                        $('.tts').attr('src', "data:audio/ogg;base64," + obj.payload.toString());
+                        $('.stt').html(obj.text.toString());
+                        $("#mainDiv").removeClass("loader");
+                        recordingButton.show();
+                    },
+                    error: function(err) {
+                        console.log("Error: " + err);
+                        $('.stt').html(obj.text.toString());
+                        $("#mainDiv").removeClass("loader");
+                        recordingButton.show();
+                    }
+                });
+            }
+        }
+        mediaRecorder.start();
+        mediaRecorder.pause();
+    }
+
     // Call one time record to get the user agreement for using the microphone
-    Fr.voice.record(false);
-    Fr.voice.pause();
-    Fr.voice.recorder.clear();
+    window.AudioContext = window.AudioContext||window.webkitAudioContext;
+    navigator.getUserMedia = navigator.getUserMedia || navigator.webkitGetUserMedia
+        || navigator.mozGetUserMedia || navigator.msGetUserMedia;
+    window.URL = window.URL || window.webkitURL;
+
+    navigator.getUserMedia({audio: true}, gotStream, function() {
+        alert('No live audio input');
+    });
 });
 
 $(document).ready(function() {
-
+    var recordingButton = $(".btn-circle");
     $("#record").click(function () {
-        var $recordingButton = $(".btn-circle");
-
         if(!recording) {
-            $recordingButton.css('background-color', '#FF7F50').addClass("recording");
+            recordingButton.css('background-color', '#FF7F50').addClass("recording");
             recording = true;
-            Fr.voice.resume();
+            mediaRecorder.resume();
         } else {
-            $recordingButton.css('background-color', '#e6e6e6').removeClass("recording").hide();
-            $("#mainDiv").addClass("loader");
+            recordingButton.css('background-color', '#e6e6e6').removeClass("recording");
             recording = false;
-            Fr.voice.pause();
-            Fr.voice.export(function(blob){
-
-                    var payload = {"payload": blob};
-                        $.ajax({
-                            //url: 'https://speech.googleapis.com/v1/speech:recognize?key=API_KEY',
-                            url: 'https://openwhisk.ng.bluemix.net/api/v1/web/Hochschule_Test/default/Router.http' ,
-                            //url: 'https://openwhisk.ng.bluemix.net/api/v1/web/Hochschule_Test/default/speechToText.http',
-                        //url : 'https://openwhisk.ng.bluemix.net/api/v1/web/kuar1013_kuar1013-Sued/default/speech-to-text.http',
-                        type: 'POST',
-                        data: JSON.stringify(payload),
-                            /* Example object for Google Cloud Speech Service
-                             {
-                             "config": {
-                             "encoding": "LINEAR16",
-                             "sampleRateHertz": 16000,
-                             "languageCode": "en-US"
-                             },
-                             "audio": {
-                             "content": "/9j/7QBEUGhvdG9zaG9...base64-encoded-audio-content...fXNWzvDEeYxxxzj/Coa6Bax//Z"
-                             }
-                             }*/
-                        contentType: "application/json",
-                        processData: false,
-                        success: function(data) {
-                            console.log(data);
-                            $("#mainDiv").removeClass("loader");
-                            $recordingButton.show();
-                            var obj = JSON.parse(data);
-                            //obj = JSON.stringify(obj.payload);
-                            console.log("Data: " + data);
-                            $('.tts').attr('src', "data:audio/ogg;base64," + obj.payload.toString());
-                            $('.stt').html(obj.text.toString());
-
-
-                        },
-                        error: function(err) {
-                            console.log(err);
-                            $("#mainDiv").removeClass("loader");
-                            $recordingButton.show();
-                            $('.stt').html(obj.text.toString());
-                        }
-                    });
-                    console.log(blob);
-                   /* var audio = document.createTextNode('audio');
-                    audio.src = base64;
-                    audio.controls = true;
-                    audio.style.display = 'block';
-                    document.appendChild(audio),
-                    audio.play(); */
-
-                }, "base64");
-            Fr.voice.recorder.clear();
-            }
-    }
-
-    )
-
-
+            mediaRecorder.requestData();
+            mediaRecorder.pause();
+        }
+    });
 });
 
 
