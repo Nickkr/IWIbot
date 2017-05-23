41c5bd3c2667294d94b2454a1c05d7266542137d
Niklas Kreutzarek <nkreutzarek@hotmail.de>
2017-05-23 23:18:20 +0200

    added check if htmlText value is undefined

--- IWIbotWebServer/public/javascripts/bundle.js
+++ IWIbotWebServer/public/javascripts/bundle.js
@@ -8822,7 +8822,7 @@ var exports = module.exports = {};
 
 exports.con = function (param) {
     console.log("Param :" + param);
-    param = {"transcript" : param.toString()};
+    param = {"transcript": param.toString()};
     var options = {
         url: 'https://openwhisk.ng.bluemix.net/api/v1/web/Hochschule_Test/default/RouterV2.http',
         type: 'POST',
@@ -8832,14 +8832,17 @@ exports.con = function (param) {
         success: function (data) {
             var obj = JSON.parse(data);
             console.log("DATA " + data);
-            console.log("HTML: " + obj.htmlText );
+            console.log("HTML: " + obj.htmlText);
             valueRecived = JSON.parse(data);
             htmlText = valueRecived.htmlText;
             valueRecived = valueRecived.payload.toString();
             var msgRecived = '<div class="row msg "><div class="col-lg-3"></div><div class="col-lg-4"><div class="msg-recived">' + valueRecived + '</div></div><div class="col-lg-5"></div></div>';
             $(msgRecived).appendTo("#chat div.container").hide().fadeIn();
-            var html = '<div class="row msg "><div class="col-lg-3"></div><div class="col-lg-4"><div class="html-recived">'+ htmlText+ '</div></div><div class="col-lg-5"></div></div>';
-            $(html).appendTo("#chat div.container").hide().fadeIn();
+            if (typeof htmlText !== 'undefined') {
+
+                var html = '<div class="row msg "><div class="col-lg-3"></div><div class="col-lg-4"><div class="html-recived">' + htmlText + '</div></div><div class="col-lg-5"></div></div>';
+                $(html).appendTo("#chat div.container").hide().fadeIn();
+            }
             window.scrollTo(0, document.body.scrollHeight);
 
         },
