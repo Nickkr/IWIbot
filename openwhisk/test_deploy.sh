b6e220f380a8b4d908d37722d45eafa721220a80
Niklas Kreutzarek <nkreutzarek@hotmail.de>
2017-05-29 16:57:37 +0200

    added travis test script

---
+++ openwhisk/test_deploy.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+COMMAND=$1
+
+if [ "$1" = "" ]; then
+
+    echo "Usage:"
+	echo " bash deploy.sh [Command]"
+	echo ""
+	echo "Commands:"
+	echo " --install		All .js actions in the directory are created in OpenWhisk"
+	echo " --uninstall		All .js actions in the directory are deleted in OpenWhisk"
+	echo " --update			All .js actions in the directroy are updated/created in OpenWhisk"
+    exit
+fi
+
+
+if [ "$COMMAND" = "--install" ]; then
+    echo "Initiating deploy sequence..."
+    SAVEIFS=$IFS
+    IFS=$(echo -en "\n\b")
+    for file in *; do
+      wsk action create test/${file/.js/} $file --web true;
+    done
+    IFS=$SAVEIFS
+    echo "Deployment is done"
+    exit
+elif [ "$COMMAND" = "--uninstall" ]; then
+    echo "Initiating delete sequence..."
+    SAVEIFS=$IFS
+    IFS=$(echo -en "\n\b")
+    for file in *; do
+      wsk action delete test/${file/.js/};
+    done
+    IFS=$SAVEIFS
+    echo "Deployment is done"
+	  exit
+elif [ "$COMMAND" = "--update" ]; then
+    echo "Initiating update sequence..."
+    SAVEIFS=$IFS
+    IFS=$(echo -en "\n\b")
+    for file in *; do
+      wsk action update test/${file/.js/} $file --web true;
+    done
+    IFS=$SAVEIFS
+    echo "Deployment is done"
+	  exit
+fi
