56f7d764bb6789aaf6eef268a4904ffe7ddb586d
Niklas Kreutzarek <nkreutzarek@hotmail.de>
2017-06-01 21:25:11 +0200

    removed unused views

--- routers/index.js
+++ routers/index.js
@@ -3,16 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-    res.render('stream', { title: 'IWIBot!' });
-});
-/* GET home page. */
-router.get('/textInput', function(req, res) {
-    res.render('textInput', { title: 'IWIBot' });
+    res.render('index', { title: 'IWIBot!' });
 });
 
-router.get('/stream', function(req, res) {
-    res.render('stream', { title: 'IWIBot' });
-});
 
 module.exports = router;
 
