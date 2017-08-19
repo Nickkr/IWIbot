ffb05886de2c330295d0c356bd205345740eec3d
Niklas Kreutzarek <nkreutzarek@hotmail.de>
2017-08-19 14:21:38 +0200

    Clear Login data from form after close or submit

--- openwhisk/router/lib/conversation.js
+++ openwhisk/router/lib/conversation.js
@@ -3,8 +3,9 @@ var conversation = new ConversationV1({
     username: "15bdf076-1ad9-4063-9bb1-6eb1db935f39",
     password: "8B5qyZwIftVg",
     path: {workspace_id: '8f7c31cd-6f9b-4bcb-bca8-f79ef7689085'},
-    version_date: "2017-13-06"
-    // 6/13/2017
+    version_date: "2017-19-08"
+    //2017-13-06
+    // 6/13/20171
     //"2017-02-03"
 });
 
