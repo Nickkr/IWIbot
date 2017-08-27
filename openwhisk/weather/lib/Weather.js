1b012dbeac43ffe15af26c613c9580a456232b49
Armin Kunkel <armin@kunkel24.de>
2017-08-27 16:31:34 +0200

    fixed weather action

--- openwhisk/weather/lib/Weather.js
+++ openwhisk/weather/lib/Weather.js
@@ -23,6 +23,8 @@ function main(params) {
     console.log('WeatherAction Params:' + JSON.stringify(params));
     console.log('Day: ' + currentDay);
     console.log('DayString: ' + currentDayString);
+    params.units = 'm';
+    params.language = 'de-DE';
 
     if('latitude' in params && 'longitude' in params) {
         geocode[0] = params.latitude;
@@ -50,22 +52,32 @@ function main(params) {
                 var forecasts = result.forecasts, forecastsLength = result.forecasts.length;
                 var ul = '<ul>';
 
-                for(var i = 0; i < forecastsLength ; i++) {
+                for (var i = 0; i < forecastsLength ; i++) {
                     var forecast = forecasts[i];
                     var day = forecast.day, night = forecast.night, lunar_phase = forecast.lunar_phase;
-                    ul += '<li><b>' + day.daypart_name + '(' + day.long_daypart_name + '):</b>' + ' ' + day.narrative + '</li>';
+                    ul += '<li>';
+                    if ('day' in forecast)
+                        ul += '<b>' + day.daypart_name + '(' + day.long_daypart_name + '):</b>' + ' ' + day.narrative + '</br>';
+                    if ('night' in forecast)
+                        ul += '<b>' + night.daypart_name + '(' + night.long_daypart_name + '):</b>' + ' ' + night.narrative;
+                    ul += '</li>';
                 }
 
                 ul += '</ul>';
 
                 resultObject.htmlText = ul;
-                resultObject.payload = 'In ' + geo_location + ' wird es heute ' + result.forecasts[0].day.shortcast;
+                var shortcast = undefined;
+                if ('day' in forecasts[0]) {
+                    shortcast = forecasts[0].day.shortcast;
+                } else {
+                    shortcast = forecasts[0].night.shortcast;
+                }
+                resultObject.payload = 'In ' + geo_location + ' wird es heute ' + shortcast;
 
                 resolve(resultObject);
 
             } else {
                 console.log('http status code:', (result || {}).statusCode);
-                console.log('error:', error);
                 console.log('result:', result);
                 reject(error);
             }
