b142670bf2e0d79e2762aad86d34e7b58f4040d8
Armin Kunkel <armin@kunkel24.de>
2017-09-12 12:55:59 +0200

    updated version date

--- openwhisk/router/lib/conversation.js
+++ openwhisk/router/lib/conversation.js
@@ -3,7 +3,7 @@ var conversation = new ConversationV1({
     username: "778dd9b2-02fb-4b22-895f-5c4b0b7a763b",
     password: "OKlE4wpgYNpb",
     path: {workspace_id: '49d2a377-47a0-42aa-9649-cbce4637b624'},
-    version_date: "2017-30-08"
+    version_date: "2017-12-09"
     //2017-13-06
     // 6/13/20171
     //"2017-02-03"
