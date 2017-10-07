7135c22193ba71a3e79428454d7a4a020f2fdd53
Armin Kunkel <armin@kunkel24.de>
2017-10-07 20:22:03 +0200

    changed conversation service to use vcaps services

--- openwhisk/router/lib/conversation.js
+++ openwhisk/router/lib/conversation.js
@@ -1,13 +1,11 @@
 var ConversationV1 = require('watson-developer-cloud/conversation/v1');
-var conversation = new ConversationV1({
-    username: "778dd9b2-02fb-4b22-895f-5c4b0b7a763b",
-    password: "OKlE4wpgYNpb",
+var vcapServices = require('vcap_services');
+var extend = (extend = require('util')._extend);
+
+var conversation = new ConversationV1(extend({
     path: {workspace_id: '49d2a377-47a0-42aa-9649-cbce4637b624'},
     version_date: "2017-12-09"
-    //2017-13-06
-    // 6/13/20171
-    //"2017-02-03"
-});
+}, vcapServices.getCredentials('conversation')));
 
 exports.sendMessage = function sendMessage(init, params) {
     console.log("------Conversation Started!------");
