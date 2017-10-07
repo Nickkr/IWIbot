c72497ef742c762bdc1e0082772af68077868459
Armin Kunkel <armin@kunkel24.de>
2017-10-07 20:26:55 +0200

    deleted url

--- openwhisk/router/lib/conversation.js
+++ openwhisk/router/lib/conversation.js
@@ -2,10 +2,14 @@ var ConversationV1 = require('watson-developer-cloud/conversation/v1');
 var vcapServices = require('vcap_services');
 var extend = (extend = require('util')._extend);
 
+var tmp = {};
+tmp.username = vcapServices.getCredentials('conversation').username;
+tmp.password = vcapServices.getCredentials('conversation').password;
+
 var conversation = new ConversationV1(extend({
     path: {workspace_id: '49d2a377-47a0-42aa-9649-cbce4637b624'},
     version_date: "2017-12-09"
-}, vcapServices.getCredentials('conversation')));
+}, tmp));
 
 exports.sendMessage = function sendMessage(init, params) {
     console.log("------Conversation Started!------");
