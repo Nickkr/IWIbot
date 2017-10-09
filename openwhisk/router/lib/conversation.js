1308b70ece5d3341ccda9a44a8b9f5fc1410fac8
Armin Kunkel <armin@kunkel24.de>
2017-10-09 20:35:30 +0200

    corrected a mistake the second

--- openwhisk/router/lib/conversation.js
+++ openwhisk/router/lib/conversation.js
@@ -1,7 +1,7 @@
 var ConversationV1 = require('watson-developer-cloud/conversation/v1');
 
 var conversation = new ConversationV1({
-    username: "778dd9b2-02fb-4b22-895f-5c4b0b7a763b",
+    username: $CONVERSATION_USERNAME,//"778dd9b2-02fb-4b22-895f-5c4b0b7a763b",
     password: "OKlE4wpgYNpb",
     path: {workspace_id: '49d2a377-47a0-42aa-9649-cbce4637b624'},
     version_date: "2017-12-09"
