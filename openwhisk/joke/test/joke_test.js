b545939d78f99878804dc4319630f95e09fa09d3
Voldemord92 <armin@kunkel24.de>
2017-12-01 14:20:08 +0100

    add static api_code for tests and bind services to application

--- openwhisk/joke/test/joke_test.js
+++ openwhisk/joke/test/joke_test.js
@@ -2,7 +2,8 @@
  * Created by Armin on 05.06.2017.
  */
 var request = require('request');
-var actionUrl = 'https://service.us.apiconnect.ibmcloud.com/gws/apigateway/api/'+process.env.WSK_API_CODE+'/iwibotTest/joke';
+//var actionUrl = 'https://service.us.apiconnect.ibmcloud.com/gws/apigateway/api/'+process.env.WSK_API_CODE+'/iwibotTest/joke';
+var actionUrl = 'https://service.us.apiconnect.ibmcloud.com/gws/apigateway/api/c9f88de3acb5a4648e4f118769d019c8df8797d1777c4342f43260626b4c51bf/iwibotTest/joke';
 
 module.exports = {
     'Joke Action Test' : function (test) {
@@ -11,6 +12,15 @@ module.exports = {
             console.log('Body: ' + body);
             console.log('Error: ' + err);
             console.log('Response: ' + JSON.stringify(response));
+            console.log('\n\n\nTypeof process: '+ typeof process +'\n\n\n');
+            if(typeof process == "object") {
+                console.log('\n\n\nTypeof env: '+ typeof process.env +'\n\n\n');
+
+                if(typeof process.env == "obejct") {
+                    console.log('\n\n\nEnv vars: '+ JSON.stringify(process.env) +'\n\n\n');
+                }
+            }
+            console.log('\n\n\n \n\n\n');
             body = JSON.parse(body);
             test.ok('payload' in body);
             test.done();
